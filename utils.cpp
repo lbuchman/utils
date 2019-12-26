42af2536f2825bd7b86f97b3f4fccfd59f21a02b
Leo Buchman <leo.buchman@lenel.com>
2019-12-26 04:33:42 -0500

    auto

--- utils.cpp
+++ utils.cpp
@@ -6,13 +6,13 @@
 /* return temp in C   */
 /****************************************************************************/
 float moving_window_average(long int dat, Window& window) {
+
     window.window_total -= window.window[window.window_idx];
     window.window_total += dat;
     window.window[window.window_idx] = dat;
     window.window_idx = (window.window_idx < (window.size - 1)) ? (window.window_idx + 1) : 0;
-    return (window.window_total / window.size);
+    return (float)(1.0 * (float) window.window_total / (float) window.size);
 }
-
 /*!
  ********************************************************************
  * brief
@@ -111,9 +111,9 @@ float CheckLimits1(float min, float max, float value, String desc) {
 
 * *************************************************************
 */
-int CheckLimits(int min, int max, int value, String desc) {
+int CheckLimits(int min, int max, int value) {
     if((value < min) || (value > max)) {
-        return 0;
+        return -1;
     };
 
     return value;
--- utils.h
+++ utils.h
@@ -20,7 +20,7 @@ unsigned int doubleToBcd(double number, int hole, int frac);
 bool expired_interval(size_t& timer, size_t interval, size_t& prev_run_time, char* name, bool debug);
 double CheckLimits(double min, double max, double value, String desc);
 //float CheckLimits(float min, float max, float value);
-int CheckLimits(int min, int max, int value, String desc);
+int CheckLimits(int min, int max, int value);
 void hexdump(const void * memory, size_t bytes);
 uint32_t rotl32(uint32_t n, unsigned int c);
 uint32_t rotr32(uint32_t n, unsigned int c);
