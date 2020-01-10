a22c84fce6f2592d0309eb5dce5d469b64ae88cb
Leo Buchman <leo.buchman@lenel.com>
2020-01-10 00:12:12 -0500

    auto

---
+++ interval.h
@@ -0,0 +1,6 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+bool expired_interval(size_t& timer, size_t interval, size_t& prev_run_time, char* name, bool debug);
+
+#endif
--- utils.h
+++ utils.h
@@ -2,13 +2,20 @@
 #define UTILS_H
 
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    
 unsigned int stringToBcd(char *buffer, int hole, int frac);
 unsigned int doubleToBcd(double number, int hole, int frac);
-bool expired_interval(size_t& timer, size_t interval, size_t& prev_run_time, char* name, bool debug);
-double CheckLimits(double min, double max, double value, String desc);
-//float CheckLimits(float min, float max, float value);
 int CheckLimits(int min, int max, int value);
 void hexdump(const void * memory, size_t bytes);
 uint32_t rotl32(uint32_t n, unsigned int c);
 uint32_t rotr32(uint32_t n, unsigned int c);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
